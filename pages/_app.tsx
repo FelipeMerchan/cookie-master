2a4b52f5863ca19295c6bae6a7a286e44aa8fa4b
Felipe Merchan <felipe-sc@hotmail.es>
2022-07-31 12:32:29 -0500

    168. Generar la pagina con el tema solicitado

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import type { AppContext, AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { CssBaseline, Theme, ThemeProvider } from '@mui/material'
 
-import { darkTheme } from '../themes'
+import { customTheme, darkTheme, lightTheme } from '../themes'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps, ...rest }: AppProps) {
-  console.log({ rest })
+interface Props extends AppProps {
+  theme: string;
+}
+
+function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
+  const currentTheme: Theme = theme === 'light'
+    ? lightTheme
+    : (theme === 'dark')
+      ? darkTheme
+      : customTheme
 
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={currentTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
@@ -16,16 +24,18 @@ function MyApp({ Component, pageProps, ...rest }: AppProps) {
 }
 
 /* getInitialProps no es recomendado usarlo. Es mas recomendado
-  usar getServerSideProps y getStaticProps en lugar de getInitialProps
+  usar getServerSideProps y getStaticProps en lugar de getInitialProps.
+  Cuando implementamos getInitialProps en el archivo _app.tsx se desactiva
+  la optimizacion automatica de static generation
 */
 
-MyApp.getInitialProps = async (appContext: AppContext) => {
+/* MyApp.getInitialProps = async (appContext: AppContext) => {
   const { theme } = appContext.ctx.req ? (appContext.ctx.req as any).cookies : { theme: 'light' }
   const validThemes = ['light', 'dark', 'custom']
 
   return {
     theme: validThemes.includes(theme) ? theme : 'dark',
   }
-}
+} */
 
 export default MyApp
