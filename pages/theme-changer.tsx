954396a8cb2f0072694db6b7f18440e05bad30f7
Felipe Merchan <felipe-sc@hotmail.es>
2022-07-30 22:54:31 -0500

    165. Leer las cookies

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,10 +1,12 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, FC, useState } from 'react'
+import { GetServerSideProps } from 'next'
 import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
 import Cookies from 'js-cookie'
 
 import { Layout } from '../components/layouts'
 
-const ThemeChangerPage = () => {
+const ThemeChangerPage: FC = ({ theme, name }) => {
+    console.log(theme, name)
     const [currentThem, setCurrentThem] = useState('light')
     const onThemeChange = (event: ChangeEvent<HTMLInputElement>) => {
         const selectedTheme = event.target.value
@@ -35,6 +37,19 @@ const ThemeChangerPage = () => {
             </Card>
         </Layout>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    /* La req es la solicitud del cliente, es decir, cuando la persona ingresa a la URL
+    del sitio se realiza un request hacia el backend */
+    const { theme = 'light', name = 'No name' } = req.cookies
+
+    return {
+        props: {
+            theme,
+            name,
+        }
+    }
 }
 
 export default ThemeChangerPage
